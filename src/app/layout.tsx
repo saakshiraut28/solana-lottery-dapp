885ee5e8aa7f3c81b86323dcc29687a04a28b769
Saakshi Raut <saakshiraut28@gmail.com>
2024-09-18 01:31:03 +0530

    add: connect wallet

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import AppWalletProvider from "./components/AppWalletProvider";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <AppWalletProvider>{children}</AppWalletProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+/** @format */
+
+"use client";
+
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <div className="border hover:border-slate-900 rounded">
+        <WalletMultiButton style={{}} />
+      </div>
+    </main>
+  );
+}
