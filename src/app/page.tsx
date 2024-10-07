514f1b07532ac3c9c3f93dd0a8c72a8e0889e149
Saakshi Raut <saakshiraut28@gmail.com>
2024-10-07 14:27:36 +0530

    work on frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
     <div className="min-h-screen flex flex-col text-black">
       <Navbar />
       <div className="flex flex-col items-center justify-center py-12 opacity-75 font-main">
-        <div className="md:w-1/2 w-full bg-gray-800 border border-white rounded-lg p-6">
+        <div className="md:w-1/2 w-full  border border-white rounded-lg p-6">
           <p className="text-lg mb-4">
             <span className="font-bold">Lottery ID:</span> {lotteryId}
           </p>
