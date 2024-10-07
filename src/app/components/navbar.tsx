a396a6e8efe180050be53fc10782bf6ed55beee4
Saakshi Raut <saakshiraut28@gmail.com>
2024-10-07 14:31:02 +0530

    work on the navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -5,7 +5,7 @@ import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 const Navbar = () => {
   return (
     <>
-      <div className="flex items-center justify-between px-4 py-2 bg-white opacity-75">
+      <div className="absolute top-0 flex items-center justify-between px-4 py-2 bg-white opacity-75">
         <div className="font-main text-3xl">
           Soltery | Win-Win for everyone! :)
         </div>
